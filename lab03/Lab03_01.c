31745868faa3050471ccf18fc00affdd366a39b6
mettlmi1 <mettlermicha@gmail.com>
2024-03-05 09:36:16 +0100

    finished lab03 part 1

--- lab03/Lab03_01.c
+++ lab03/Lab03_01.c
@@ -2,19 +2,20 @@
 #include <stdio.h>
 
 int main() {
+    //unsigned int number = 0x75;
     unsigned int number = 0x75;
     unsigned int bit = 3; // bit at position 3
 
     // Setting a bit
-    number = ...;
+    number = number | (1 << bit);
 
     // Clearing a bit
     bit = 1;
-    number = ...;
+    number = number & ~(1 << bit);
 
     // Toggling a bit
     bit = 0;
-    number = ...;
+    number = number ^ (1 << bit);
 
     printf("number = 0x%02X\n", number);
 
