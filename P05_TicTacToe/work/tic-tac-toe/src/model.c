f4df3bd9f0f1de3e4148cda5e2f04f2197ce3242
mettlmi1 <mettlermicha@gmail.com>
2024-03-26 07:56:37 +0000

    finished lab 5 task 2

--- P05_TicTacToe/work/tic-tac-toe/src/model.c
+++ P05_TicTacToe/work/tic-tac-toe/src/model.c
@@ -29,11 +29,8 @@ static void set_state(model_t *instance, model_pos_t pos, model_state_t state)
     // set the field of the board to the new state
     // BEGIN-STUDENTS-TO-ADD-CODE
 
+    instance->board[pos.row][pos.col] = state;
 
-    
-
-
-    
     // END-STUDENTS-TO-ADD-CODE
 }
 
@@ -52,10 +49,13 @@ void model_init(model_t *instance)
     // set all fields of the board to model_state_none
     // BEGIN-STUDENTS-TO-ADD-CODE
 
-
-
-
-
+    for (int row = 0; row < MODEL_SIZE; row++)
+    {
+        for (int col = 0; col < MODEL_SIZE; ++col)
+        {
+            instance->board[row][col] = model_state_none;
+        }
+    }
 
     // END-STUDENTS-TO-ADD-CODE
 }
@@ -70,9 +70,7 @@ model_state_t model_get_state(model_t *instance, model_pos_t pos)
     // replace the stub implementation my access to the field at the given position.
     // BEGIN-STUDENTS-TO-ADD-CODE
 
-
-    return model_state_none; // stub 
-
+    return instance->board[pos.row][pos.col]; // stub
 
     // END-STUDENTS-TO-ADD-CODE
 }
@@ -82,42 +80,43 @@ model_line_t model_get_win_line(model_t *instance)
 {
     assert(instance);
     model_state_t anchor;
-    
+
     // horizontal
-    for(size_t row = 0; row < MODEL_SIZE; row++) {
+    for (size_t row = 0; row < MODEL_SIZE; row++)
+    {
         anchor = model_get_state(instance, model_pos(row, 0));
-        if (anchor != model_state_none
-            && anchor == model_get_state(instance, model_pos(row, 1))
-            && anchor == model_get_state(instance, model_pos(row, 2))) {
-            return (model_line_t) { model_dir_h, { row, 0 } };
+        if (anchor != model_state_none && anchor == model_get_state(instance, model_pos(row, 1)) && anchor == model_get_state(instance, model_pos(row, 2)))
+        {
+            return (model_line_t){model_dir_h, {row, 0}};
         }
     }
-    
+
     // vertical
-    for(size_t col = 0; col < MODEL_SIZE; col++) {
+    for (size_t col = 0; col < MODEL_SIZE; col++)
+    {
         anchor = model_get_state(instance, model_pos(0, col));
-        if (anchor != model_state_none
-            && anchor == model_get_state(instance, model_pos(1, col))
-            && anchor == model_get_state(instance, model_pos(2, col))) {
-            return (model_line_t) { model_dir_v, { 0, col } };
+        if (anchor != model_state_none && anchor == model_get_state(instance, model_pos(1, col)) && anchor == model_get_state(instance, model_pos(2, col)))
+        {
+            return (model_line_t){model_dir_v, {0, col}};
         }
-
-
     }
-    
+
     // diagonal
     anchor = model_get_state(instance, model_pos(1, 1));
-    if (anchor != model_state_none) {
-        if (anchor == model_get_state(instance, model_pos(0, 0)) && anchor == model_get_state(instance, model_pos(2, 2))) {
-            return (model_line_t) { model_dir_d, { 0, 0 } };
+    if (anchor != model_state_none)
+    {
+        if (anchor == model_get_state(instance, model_pos(0, 0)) && anchor == model_get_state(instance, model_pos(2, 2)))
+        {
+            return (model_line_t){model_dir_d, {0, 0}};
         }
-        if (anchor == model_get_state(instance, model_pos(2, 0)) && anchor == model_get_state(instance, model_pos(0, 2))) {
-            return (model_line_t) { model_dir_d, { 0, 2 } };
+        if (anchor == model_get_state(instance, model_pos(2, 0)) && anchor == model_get_state(instance, model_pos(0, 2)))
+        {
+            return (model_line_t){model_dir_d, {0, 2}};
         }
     }
 
     // fallback
-    return (model_line_t) { model_dir_none, { 0, 0 } }; 
+    return (model_line_t){model_dir_none, {0, 0}};
 }
 
 // public API function which is documented in the header file.
@@ -127,26 +126,29 @@ model_state_t model_get_winner(model_t *instance)
     model_line_t line = model_get_win_line(instance);
     return line.dir == model_dir_none
         ? model_state_none
-        : model_get_state(instance, model_pos(line.start.row, line.start.col))
-        ;
+               : model_get_state(instance, model_pos(line.start.row, line.start.col));
 }
 
 // public API function which is documented in the header file.
 int model_can_move(model_t *instance)
 {
     assert(instance);
-    if (model_get_winner(instance) == model_state_none) {
+    if (model_get_winner(instance) == model_state_none)
+    {
         // Instructions to the students:
         // scan all fields: return 1 with first field which equals model_state_none
         // BEGIN-STUDENTS-TO-ADD-CODE
 
-
-
-
-
-
-
-
+        for (int row = 0; row < MODEL_SIZE; row++)
+        {
+            for (int col = 0; col < MODEL_SIZE; ++col)
+            {
+                if (instance->board[row][col] == model_state_none)
+                {
+                    return 1;
+                }
+            }
+        }
         // END-STUDENTS-TO-ADD-CODE
     }
     return 0;
@@ -157,7 +159,8 @@ int model_move(model_t *instance, model_pos_t pos, model_state_t state)
 {
     assert(instance);
     assert_pos(pos);
-    if (model_get_state(instance, pos) == model_state_none && model_can_move(instance)) {
+    if (model_get_state(instance, pos) == model_state_none && model_can_move(instance))
+    {
         set_state(instance, pos, state);
         return 1;
     }
